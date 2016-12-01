8453fb5f2ba5aedd984c03ba299c57c5cb21dc6d
Zexuz <Robin.edbom@gmail.com>
2016-12-01 20:32:52 +0100

    Changed mapStructure

--- Day1/Properties/AssemblyInfo.cs
+++ Day1/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("AdventOfCode")]
+[assembly: AssemblyTitle("Day1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdventOfCode")]
+[assembly: AssemblyProduct("Day1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
