e7fb01876f332b34945afbde81f20f0e7cea51e4
Zexuz <Robin.edbom@gmail.com>
2016-12-04 14:48:34 +0100

    Solve for day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day4
+{
+    internal class Program
+    {
+        private string _inputFile;
+
+        private void Start()
+        {
+            _inputFile = File.ReadAllText("../../input data");
+            var lines = _inputFile.Split('\n');
+            var regEx = new Regex(@"(\D+)-(\d+)\[(\D+)\]");
+
+            var sumOfSectorIds = lines
+                .Select(line => new Room(line, regEx))
+                .Where(room => room.IsValidRoom())
+                .Sum(room => room.SectorId);
+
+            Console.WriteLine(sumOfSectorIds);
+        }
+
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+    }
+
+    internal class Room
+    {
+        public string Name { get; }
+        public int SectorId { get; }
+        public string CheckSum { get; }
+
+        public Room(string rawRoomString, Regex regex)
+        {
+            var match = regex.Match(rawRoomString);
+
+            Name = match.Groups[1].Value.Replace("-", "");
+            SectorId = int.Parse(match.Groups[2].Value);
+            CheckSum = match.Groups[3].Value;
+        }
+
+        public bool IsValidRoom()
+        {
+            var strGroupedByChars = Name
+                .GroupBy(c => c)
+                .OrderByDescending(c => c.ToList().Count)
+                .ThenBy(c => c.Key)
+                .ToList();
+            for (var i = 0; i < CheckSum.Length; i++)
+            {
+                if(CheckSum[i] == strGroupedByChars[i].Key) continue;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
