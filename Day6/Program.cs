812e322ba5b8f9923a44d1ad633cf5b94e668787
Zexuz <Robin.edbom@gmail.com>
2016-12-06 18:06:07 +0100

    Solve for day 6 part1

---
+++ Day6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Day6
+{
+    internal class Program
+    {
+        private void Start()
+        {
+            var inputFile = File.ReadAllText("../../input data");
+            var rows = inputFile.Split('\n');
+
+            var dict = new Dictionary<int, string>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                dict.Add(i, "");
+                var rowIndex = 0;
+                while (rows.Length > rowIndex)
+                {
+                    dict[i] += rows[rowIndex][i].ToString();
+                    rowIndex++;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+                Console.Write(dict[i].GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key);
+        }
+
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+    }
+}
